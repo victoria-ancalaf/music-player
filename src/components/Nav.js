02f9dc2235697710e13ea19173441aaa845d43f0
Victoria Ancalaf <victoria@cloud101.co>
2023-01-24 23:13:46 -0300

    add icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import { faMusic } from "@fortawesome/free-solid-svg-icons";
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
    return (
       <nav>
-         <h1>Waves</h1>
+         <h1>Music Player</h1>
          <button onClick={() => setLibraryStatus(!libraryStatus)}>
             Library
             <FontAwesomeIcon icon={faMusic} style={{ paddingLeft: "5px" }} />
